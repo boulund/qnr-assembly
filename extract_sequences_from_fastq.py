db935250adef9f4115992093b84e6aab25076adc
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-03-09 13:23:58 +0100

    Made it iteratively go through the correct FASTQ file looking for both pairs of each read.

--- extract_sequences_from_fastq.py
+++ extract_sequences_from_fastq.py
@@ -2,7 +2,10 @@
 # Extract sequences matching qnr HMM from FASTQ files
 # using headers from FASTA.
 
-from sys import argv, exit
+from sys import argv, exit, stdout, stderr
+from os import listdir, path
+from collections import defaultdict
+from itertools import chain
 import argparse
 
 from read_fasta import read_fasta, read_fastq
@@ -16,19 +19,32 @@ def parse_args(argv):
     parser.add_argument("-a", "--fasta", nargs="+", dest="fasta",
             required=True,
             help="FASTA file(s) to read headers from.")
-    parser.add_argument("-q", "--fastq", nargs="+", dest="fastq",
+    parser.add_argument("-q", "--fastq-dir", dest="fastq_dir",
             required=True,
-            help="FASTQ file(s) to read headers from.")
+            help="FASTQ directory to read sequences from.")
+    parser.add_argument("-o", "--output", dest="output", metavar="FILE",
+            default="",
+            help="Filename to write interleaved FASTQ output to, prints to STDOUT if not specified.")
+   # parser.add_argument("-1", "--left", dest="left",
+   #         default="left_reads.fastq",
+   #         help="Destination FASTQ file for 'left' reads [%(default)s].")
+   # parser.add_argument("-2", "--right", dest="right",
+   #         default="right_reads.fastq",
+   #         help="Destination FASTQ file for 'right' reads [%(default)s].")
         
     return parser.parse_args()
 
 
-def transform_fasta_header(header):
+def transform_fasta_header(fastaheader):
     """
     Do some string operations on FASTA headers to make them look like FASTQ
     headers.
     """
-    return "HWI"+header.split(" ", 1)[0].split("_HWI")[1].translate(str.maketrans("_", ":"))
+    fastq_source, read = fastaheader.split(" ", 1)[0].split("_HWI")
+    read_header = "HWI"+read.translate(str.maketrans("_", ":"))
+    fastq_base = fastq_source.rsplit("_", 1)[0]
+    return fastq_base, read_header
+    
 
 
 def main(options):
@@ -36,19 +52,43 @@ def main(options):
     Main function.
     """
 
-    fasta_headers = set()
-    headeradd = fasta_headers.add
+    # Construct a dictionary with FASTQ basenames, i.e. not including the pair
+    # information (e.g. _1.fasta or _2.fasta) as keys. Values are lists of the first
+    # space delimited read headers that should be extracted from the FASTQ files.
+    fastq_bases = defaultdict(list)
     for fastafile in options.fasta:
         for header, _ in read_fasta(fastafile):
-            headeradd(transform_fasta_header(header))
-    for fastqfile in options.fastq:
-        for header, seq, second_header, scores in read_fastq(fastqfile):
-            clean_header = header[1:].split(" ", 1)[0]
-            if clean_header in fasta_headers:
-                print(header)
-                print(seq)
-                print(second_header)
-                print(scores)
+            fastq_base, read_header = transform_fasta_header(header)
+            fastq_bases[fastq_base].append(read_header)
+    print("INFO: Number of keys (fastq_basenames) in header dict: {}".format(len(fastq_bases)), file=stderr)
+    print("INFO: Number of headers (reads) in dict: {}".format(len(list(chain(*fastq_bases.values())))), file=stderr)
+    
+    fastq_dir = options.fastq_dir
+    print(listdir(fastq_dir))
+    fastq_files = set(filename for filename in listdir(options.fastq_dir))
+    if options.output:
+        outfile = open(options.output, 'w')
+    else:
+        outfile = stdout
+    with outfile:
+        for fastq_base, read_headers in fastq_bases.items():
+            fastq_left = fastq_base+"_1.fastq"
+            fastq_right = fastq_base+"_2.fastq"
+            if fastq_left in fastq_files and fastq_right in fastq_files:
+                # Iterate both FASTQ files in lock-step, making it 
+                # very easy to print out the reads in interleaved order.
+                for left, right in zip(read_fastq(path.join(fastq_dir, fastq_left)), 
+                                       read_fastq(path.join(fastq_dir, fastq_right))):
+                    print(left[0], file=outfile)
+                    print(left[1], file=outfile)
+                    print(left[2], file=outfile)
+                    print(left[3], file=outfile)
+                    print(right[0], file=outfile)
+                    print(right[1], file=outfile)
+                    print(right[2], file=outfile)
+                    print(right[3], file=outfile)
+            else:
+                print("WARNING: found no FASTQ files for {}".format(fastq_base), file=stderr)
 
 
 if __name__ == "__main__":
