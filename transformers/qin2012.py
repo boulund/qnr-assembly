8ef0aa194fb2a68353eb092b1ff6064e60a4cad6
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-03-16 13:32:52 +0100

    Qin2012: Added translation dict between SRR numbers and DOMxxx names for FASTQ files.

--- transformers/qin2012.py
+++ transformers/qin2012.py
@@ -13,13 +13,47 @@ def transform_fasta_header(fastaheader):
     FASTQ filenames look like:
       DOM012.fastq
     """
+    
+    fastq_basenames = {
+        "DLM002": "SRR341590",
+        "DLM003": "SRR341591",
+        "DLM004": "SRR341592",
+        "DLM005": "SRR341593",
+        "DLM007": "SRR341595",
+        "DLM008": "SRR341596",
+        "DLM011": "SRR341598",
+        "DOM005": "SRR341607",
+        "DOM012": "SRR341608",
+        "DOM017": "SRR341610",
+        "DOM020": "SRR341611",
+        "DOM023": "SRR341613",
+        "DOM024": "SRR341614",
+        "DOM025": "SRR341615",
+        "NLM001": "SRR341625",
+        "NLM005": "SRR341627",
+        "NOM008": "SRR341646",
+        "NOM013": "SRR341648",
+        "NOM026": "SRR341652",
+        "DLM014": "SRR341662",
+        "NLM021": "SRR341699",
+        "NLM022": "SRR341700",
+        "NLM026": "SRR341702",
+        "NLM029": "SRR341705",
+        "NLM031": "SRR341706",
+        "NLM032": "SRR341707",
+        "NOM001": "SRR341712",
+        "NOM017": "SRR341720",
+        "NOM027": "SRR341724",
+        "NOM028": "SRR341725"}
+
+
     fastq_base, read_header, _ = fastaheader.split("_", 2)
-    return fastq_base, read_header
+    return fastq_basenames[fastq_base], read_header
 
 def fastq_filename(fastq_base):
     """
     Return a pair of complete fastq filenames for fastq_base.
     """
-    return fastq_base+".fastq", fastq_base+".fastq"
+    return fastq_base+"_1.fastq", fastq_base+"_2.fastq"
 
 
