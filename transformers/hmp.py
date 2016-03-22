0e1266a796a31a168c75f31f1077edc16e3cbe58
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-03-22 10:18:43 +0100

    Fixed errors in HMP transformer.

--- transformers/hmp.py
+++ transformers/hmp.py
@@ -9,13 +9,13 @@ def transform_fasta_header(fastaheader):
     FASTA headers look like:
       SRS078176.denovo_duplicates_marked.trimmed.2_HWI-6X_10148_FC61V2D_4_111_8296_18840/2
     FASTQ headers look like:
-      HWI-6X_10148_FC61V2D_4_100_10000_14055/1
+      HWI-6X:10148:FC61V2D:4:100:10000:14055/1
     FASTQ filenames look like:
       SRS078176.denovo_duplicates_marked.trimmed.1.fastq
     """
-    fastq_source, read = fastaheader.split(" ", 1)[0].split("_HWI")
-    read_header = "HWI-"+read
-    fastq_base = fastq_source.rsplit(".", 1)[0]
+    fastq_source, read = fastaheader.split(" ", 1)[0].split("trimmed.")
+    read_header = read[2:].translate(str.maketrans("_",":"))
+    fastq_base = fastq_source+"trimmed"
     return fastq_base, read_header
 
 def fastq_filename(fastq_base):
